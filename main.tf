2a1f48cb014fce00b7366d6ef7b02275fe7ec60a
Towobolar <71108083+Towobolar@users.noreply.github.com>
2024-03-06 18:51:28 +0000

    changed bucket name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "first-bucket-abbey" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-abbey"
 
   tags = {
     Name        = "My bucket"
