317e55a60cd521af63b28023e671d2ce463f9f1f
Towobolar <71108083+Towobolar@users.noreply.github.com>
2024-03-07 13:53:21 +0000

    building vpc

--- main.tf
+++ main.tf
@@ -1,17 +1,90 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket-abbey"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name = "my Vpc"
   }
 }
 
 
-resource "aws_default_vpc" "default" {
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
   tags = {
-    Name = "Default VPC"
+    Name = "Main public subnet"
   }
 }
 
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Main private subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "main_route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  }
+
+  tags = {
+    Name = "my route table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.main_route.id
+  aws_instance_id = aws_instance.main_ec2.id
+}
+
+resource "aws_instance" "main_ec2" {
+  ami           = ami-09885f3ec1667cbfc
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "project ec2"
+  }
+}
+
+resource "aws_instance" "main_ec21" {
+  ami           = ami-09885f3ec1667cbfc
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "project second ec2"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+  aws_instance_id = aws_instance.main_ec2.id
+
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
 
