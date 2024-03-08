e14a6fef1bd8a3c9fa541bf86688cea7e39868e0
Towobolar <71108083+Towobolar@users.noreply.github.com>
2024-03-08 19:45:39 +0000

    create vpc

--- main.tf
+++ main.tf
@@ -1,93 +1,78 @@
-resource "aws_vpc" "main" {
+**********************
+*        VPC         *
+**********************
+
+resource "aws_vpc" "major-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "my Vpc"
+    name = "project vpc"
   }
 }
 
-
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = aws_vpc.major-vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-west-2a"
 
   tags = {
-    Name = "Main public subnet"
+    Name = "project public subnet"
   }
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "private-subnet" {
+  vpc_id     = aws_vpc.major-vpc.id
   cidr_block = "10.0.2.0/24"
+  availability_zone = "eu-west-2b"
 
   tags = {
-    Name = "Main private subnet"
+    Name = "project private subnet"
   }
 }
 
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.major-vpc.id
 
   tags = {
-    Name = "main"
+    Name = "internet gw"
   }
 }
 
-resource "aws_route_table" "main_route" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.major-vpc.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 
-  route {
-    ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
-  }
 
   tags = {
-    Name = "my route table"
+    Name = "internet public route"
   }
 }
 
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.main_route.id
-  aws_instance_id = aws_instance.main_ec2.id
+resource "aws_route_table_association" "public-association" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.public-rt.id
 }
 
-resource "aws_instance" "main_ec2" {
-  ami           = ami-09885f3ec1667cbfc
-  instance_type = "t2.micro"
-  subnet_id      = aws_subnet.public_subnet.id
-   vpc_id     = aws_vpc.main.id
+resource "aws_security_group" "major-vpc-sg" {
+  vpc_id = aws_vpc.major-vpc.id
 
-  tags = {
-    Name = "project ec2"
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+    cidr_blocks = ["85.255.237.87/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_instance" "main_ec21" {
-  ami           = ami-09885f3ec1667cbfc
-  instance_type = "t2.micro"
-   vpc_id     = aws_vpc.main.id
-
-  tags = {
-    Name = "project second ec2"
-  }
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
-  aws_instance_id = aws_instance.main_ec2.id
-
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-
