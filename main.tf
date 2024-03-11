6cf968fa3ad3fd5d6de80f5af106dfc233af273b
Abiodun Lasisi <lasisiabioduntowobola@gmail.com>
2024-03-11 20:36:33 +0000

    update instance

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ resource "aws_vpc" "major-vpc" {
 ********************************/
 
 resource "aws_subnet" "public-subnet" {
-  vpc_id     = aws_vpc.major-vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.major-vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "eu-west-2a"
 
   tags = {
@@ -30,8 +30,8 @@ resource "aws_subnet" "public-subnet" {
 ********************************/
 
 resource "aws_subnet" "private-subnet" {
-  vpc_id     = aws_vpc.major-vpc.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.major-vpc.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-west-2b"
 
   tags = {
@@ -82,14 +82,24 @@ resource "aws_route_table_association" "public-association" {
 *        security group        *
 ********************************/
 
-resource "aws_security_group" "major-vpc-sg" {
+resource "aws_security_group" "web-sg" {
+  name = "web-sg"
+  description = "allow inbound ssh and https traffic"
   vpc_id = aws_vpc.major-vpc.id
 
   ingress {
-    protocol  = -1
-    self      = true
-    from_port = 0
-    to_port   = 0
+    protocol    = "http"
+    self        = true
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["85.255.237.87/32"]
+  }
+
+   ingress {
+    protocol    = "tcp"
+    self        = true
+    from_port   = 22
+    to_port     = 22
     cidr_blocks = ["85.255.237.87/32"]
   }
 
@@ -104,11 +114,26 @@ resource "aws_security_group" "major-vpc-sg" {
 /*******************************
 *        ec2 instance         *
 ********************************/
-resource "aws_instance" "main-instance" {
-  ami           = "ami-0f403e3180720dd7e"
-  instance_type = "t2.micro"
+resource "aws_instance" "web-instance" {
+  ami                    = "ami-0f403e3180720dd7e"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public-subnet.id
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  key_name               = aws_key_pair.brainiac-key.id
 
   tags = {
-    Name = "ec2"
+    Name = "web ec2"
   }
 }
+
+
+/*******************************
+*        Key pair         *
+********************************/
+
+resource "aws_key_pair" "brainiac-key" {
+  key_name   = "test-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCqb6C7CS4aZdjn97tOf812QEQC7R5a6/Y1vwaXPWOOmFnkK/1UucVbLJe88YBr0vlrAHnv7nsg5b9a7Lmj/Y9Rbo2q/g6bUiHoXJEHQ5/hOe7eYVlt0Pf1dtcK7Ti22dsoOjgZ4QWXcTWB7wGc1cnmUp96AnSc12+qLJ30moeo0be62rchptPvpWlAo4Xb0jcK6Wk8pWqMO1Nem4pkfudVQNe83inJc6yjelUBdGkRVRUDeWjFGxQtFygTn5wY0ExSxcpEOCSBu75HrHb8FwNBPurn/9Umy4oaGkv8oryaEoLGv60yf+Iyge3SuYwiHlmWs9Dof877fD6tMM8Zgu6n5ujm3Oa4mlFSx2IKmQUnWGBIv+4Fuhcvb96jvxut1Wg1xb5gOmO355ce1nkboAYBLghtHi5ZN66kp7AMwE/LAO/RPVZLibZtLkf8Csu+xPpFrbAktprlAmtUUEMaaWNwemWnsBUgH5SmtgOFGUlPgb58H7H9S/Xi+bNt2THMO9k= abbey@TOWOBOLA"
+}
+
+  
